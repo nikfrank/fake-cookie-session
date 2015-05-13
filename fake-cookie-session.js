b1bdb839da5fed3452720854ada8bce79534ce6c
nik frank <nikbfrank@gmail.com>
2015-05-14 00:38:34 +0300

    ware-router version untested

--- fake-cookie-session.js
+++ fake-cookie-session.js
@@ -5,9 +5,15 @@ var BSON = require('buffalo');
 var crypto = require('crypto');
 var xor = require('bitwise-xor');
 
+var ware = require('ware-router');
+
 var hmac = crypto.createHmac("sha256", 'thisisthebigcrappytokenthing'); 
 var hash2 = hmac.update('thisisanotherbigcrappytokenthing');
 var mask = hmac.digest('hex');
+
+// rework this into the ware router format
+
+// include the angular fake cookie jar, test
 
 module.exports = function(req, res, next){
     if(req.body.cookie === 'undefined') req.body.cookie = undefined;
@@ -23,6 +29,8 @@ module.exports = function(req, res, next){
     var outbson = BSON.parse(pbuffer);
 
     req.session = outbson;
+
+
 
     var resjson = res.json;
     res.json = function(jj){
@@ -45,3 +53,39 @@ module.exports = function(req, res, next){
 
     next();
 };
+
+module.NUexports = ware([], [{
+    inbound:function(req, res, scope){
+	var mask = scope.mask;
+
+	if(req.body.cookie === 'undefined') req.body.cookie = undefined;
+
+	// this should parse and check the prepended signature
+	var pxbuffer = new Buffer(req.body.cookie||'V0dS3HI=', 'base64');
+
+	var longmask = mask;
+	while(longmask.length < 2*(pxbuffer||'').length) longmask += mask;
+
+	var pbuffer = xor(pxbuffer, new Buffer(longmask, 'hex'));
+	var outbson = BSON.parse(pbuffer);
+
+	req.session = outbson;
+	
+    },
+    outbound:function(req, res, scope, pon, json){
+	var mask = scope.mask;
+	// this should prepend a signature
+
+	var buffer = BSON.serialize(req.session||{});
+
+	var longmask = mask;
+	while(longmask.length < 2*buffer.length) longmask += mask;
+
+	var xbuffer = xor(buffer, new Buffer(longmask, 'hex'));
+	var b64 = xbuffer.toString('base64');
+
+	if(req.method === 'POST') pon.fakeCookie = b64;	
+
+	json(pon);
+    }
+}}, {mask:mask});
